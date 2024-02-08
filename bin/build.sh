e3773e4fb3eacd17f8b627b09464f1e270b0119f
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2024-02-08 13:51:35 -0600

    Filled out build script.

--- bin/build.sh
+++ bin/build.sh
@@ -1,6 +1,27 @@
-docker build \
-    --no-cache \
-    --progress plain \
-    -f ./docker/Dockerfile \
-    -t nitelite_pipeline \
-    .
+#!/bin/bash
+
+# Main command
+DOCKER_CMD="docker build"
+
+# By default we want to see more
+DOCKER_CMD+=" --progress plain"
+
+# The docker file, for convenience
+DOCKER_CMD+=" -f ./docker/Dockerfile"
+
+# The tag
+DOCKER_CMD+=" -t nitelite-pipeline"
+
+# Pass any other commands to the docker build command
+if [ $# -gt 0 ]; then
+    DOCKER_CMD+=" \"$@\""
+fi
+
+# What to build
+DOCKER_CMD+=" ."
+
+# Execute the build
+echo "Executing:"
+echo $DOCKER_CMD
+echo
+eval $DOCKER_CMD
