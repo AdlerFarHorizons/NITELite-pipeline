2bb7d541f27751aeeadb318ddd12e736a896662e
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2024-02-17 15:42:23 -0600

    Filling out convert.sh

--- bin/convert.sh
+++ bin/convert.sh
@@ -3,17 +3,13 @@
 # Function to display help message
 show_help() {
     cat << EOF
-Usage: ./mosaic.sh -c CONFIG_FILEPATH -d DATA_DIR [OPTIONS]
+Usage: ./mosaic.sh -d INPUT_DIR -o OUTPUT_DIR [OPTIONS]
 
 Options:
     -c, --config CONFIG_FILEPATH    Location of the configuration file.
                                     Must be inside either the repository
                                     or the data directory.
-    -d, --data DATA_DIR             Location of the data directory. This will
-                                    be mounted to the /data directory inside
-                                    the docker container. Any data you want to
-                                    use should be inside DATA_DIR, and any paths
-                                    in the config should be relative to this.
+    -d, --input INPUT_DIR           Location of rasters to convert.
     -i, --interactive               Instead of running the execution script,
                                     open an interactive shell inside the
                                     docker container.
@@ -33,7 +29,12 @@ while [[ $# -gt 0 ]]; do
 
     case $key in
         -d|--data)
-            DATA_DIR="$2"
+            INPUT_DIR="$2"
+            shift # past argument
+            shift # past value
+            ;;
+        -o|--output)
+            OUTPUT_DIR="$2"
             shift # past argument
             shift # past value
             ;;
@@ -60,7 +61,7 @@ done
 
 # Check the necessary arguments are provided
 if [ -z "$INTERACTIVE" ]; then
-    if [ -z "$DATA_DIR" ]; then
+    if [ -z "$INPUT_DIR" ]; then
         echo "Error: Data directory is required."
         show_help
         exit 1
@@ -73,40 +74,28 @@ fi
 # Construct docker command
 DOCKER_CMD="docker compose -f $COMPOSE_FILE"
 # The docker command itself
-DOCKER_CMD+=" up"
+DOCKER_CMD+=" run"
 
 # If not interactive, then we add a "run"
 if [ -n "$INTERACTIVE" ]; then
     DOCKER_CMD+=" -i"
 fi
 
-# # Mount the data directory
-# if [ -n "$DATA_DIR" ]; then
-#     DOCKER_CMD+=" --volume $DATA_DIR:/data"
-# fi
-
-# TODO: Delete and explain why
-# Explanation for removal: The idea behind this is to allow the user to use
-# their own version of the pipeline. However, one of the goals is to freeze the
-# used code. In addition, copying over the code requires reinstalling it too.
-# If the user wants to use their own code, they should either intentionally
-# mess with a running image separately, or they should edit and rebuild.
-# if [ -n "$COPY_CODE" ]; then
-#     # Mount the code. Only use this if you want to use your own edited code.
-#     CODE_DIR=$(realpath ..)
-#     DOCKER_CMD+=" --volume $CODE_DIR:/NITELite-pipeline"
-# fi
+# Mount the data directory
+if [ -n "$INPUT_DIR" ]; then
+    DOCKER_CMD+=" --volume $INPUT_DIR:/data"
+fi
 
 # Name of the service
-DOCKER_CMD+=" postgis"
+DOCKER_CMD+=" postgis "
 
 # The script to run inside the docker image
 if [ -z "$INTERACTIVE" ]; then
 
     # This part of the command specifies the python environment
     # (inside the docker image) to use
-    DOCKER_CMD+=""
-    # DOCKER_CMD+=" raster2pgsql"
+    DOCKER_CMD+="/bin/bash -c 'raster2pgsql /data/*.tif public.referenced'"
+    # DOCKER_CMD+=" psql"
 else
     DOCKER_CMD+=" /bin/bash"
 fi
