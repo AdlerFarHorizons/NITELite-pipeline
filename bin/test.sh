5d739e6a8e4542ea09a71f74c79896e18d8e5893
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2024-02-28 16:44:49 -0600

    My image is behaving weirdly on aws, so validating if the same issue occurs for miniconda.

--- bin/test.sh
+++ bin/test.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 # Short script to test scripting works
 
-docker pull ubuntu
-echo "Available images:"
-docker image list
-
 echo
 echo "Current system parameters:"
 uname -a
 echo
 
-echo
-echo "alpine system parameters:"
-docker run alpine uname -a
-echo
+docker compose -f ./build/docker-compose.yaml run -i nitelite-pipeline conda env list
 
-echo
-echo "miniconda system parameters:"
-docker run continuumio/miniconda3 /bin/bash -c "uname -a"
-echo
+# docker pull ubuntu
+# echo "Available images:"
+# docker image list
 
-echo
-echo "nitelite-pipeline system parameters:"
-docker compose -f ./build/docker-compose.yaml run interior-nitelite-pipeline bash -c "uname -a"
-echo
+# echo
+# echo "alpine system parameters:"
+# docker run alpine uname -a
+# echo
+# 
+# echo
+# echo "miniconda system parameters:"
+# docker run continuumio/miniconda3 /bin/bash -c "uname -a"
+# echo
+
+# echo
+# echo "nitelite-pipeline system parameters:"
+# docker compose -f ./build/docker-compose.yaml run interior-nitelite-pipeline bash -c "uname -a"
+# echo
