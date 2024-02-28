72eabc2c9815e11d3436566c86346b76e556cd44
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2024-02-28 15:44:23 -0600

    Debugging what docker container is running

--- bin/test.sh
+++ bin/test.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 # Short script to test scripting works
 
+echo "Available images:"
+docker image list
+
+echo
 echo "Current system parameters:"
 uname -a
 echo
 
 echo "nitelite-pipeline system parameters:"
-docker compose -f ./build/docker-compose.yaml run nitelite-pipeline bash -c "uname -a"
+docker compose -f ./build/docker-compose.yaml run interior-nitelite-pipeline bash -c "uname -a"
