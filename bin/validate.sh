a9d7a8fce0a722b9d61c66fa1f08cc822e84e9ce
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2024-02-28 16:33:42 -0600

    Updated validation.

--- bin/validate.sh
+++ bin/validate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Short script to validate the pipeline installation
 
-docker compose -f ./build/docker-compose.yaml run -i nitelite-pipeline conda run -n nitelite-pipeline-conda python -c "import sys; print(sys.executable)"
+docker compose -f ./build/docker-compose.yaml run -i nitelite-pipeline /bin/bash -c 'conda run -n nitelite-pipeline-conda python -c "import sys; print(sys.executable)"'
