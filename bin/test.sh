3d3fc631305563494adb1c043909f093d153e778
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2024-02-28 15:27:50 -0600

    Now trying docker compose.

--- bin/test.sh
+++ bin/test.sh
@@ -4,6 +4,7 @@
 echo "Checking docker compose"
 docker compose --help
 
+docker pull 
 docker compose -f ./build/docker-compose.yaml run -i nitelite-pipeline /bin/bash -c 'echo "Pipeline is ready to run!"'
 
 echo "The script works!"
