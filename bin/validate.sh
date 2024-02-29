8d46dd20b7c5803cbac3634b0987bee0a83c5426
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2024-02-29 15:02:33 -0600

    Fixed issue w validate; mounted the output too.

--- bin/validate.sh
+++ bin/validate.sh
@@ -2,7 +2,7 @@
 # Short script to validate the pipeline installation
 
 docker compose -f ./build/docker-compose.yaml \
-    run -i nitelite-pipeline \
+    run nitelite-pipeline \
     /bin/bash -c \
     'conda run -n nitelite-pipeline-conda python -c \
     "import sys; \
