b74628ede43e5de4e181f9ae183208fdd666c18b
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2024-03-01 01:51:46 +0000

    Added another part to mount_validate.

--- bin/mount_validate.sh
+++ bin/mount_validate.sh
@@ -23,3 +23,11 @@ docker compose -f ./aws/docker-compose.yaml \
     run nitelite-pipeline \
     /bin/bash -c \
     'tail ./night-horizons-mapmaker/night_horizons/mapmake.py'
+
+docker compose -f ./aws/docker-compose.yaml \
+    run \
+    --volume $(realpath ./config/mosaic.yml):/used-config.yml \
+    nitelite-pipeline \
+    /bin/bash -c \
+    'conda run -n nitelite-pipeline-conda \
+    python '
