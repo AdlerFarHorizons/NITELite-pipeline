e0f83d3fdcd7a1e43af163139d6c49f5f4e4f6ba
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2024-03-01 00:11:55 +0000

    Trying mount validation script.

--- bin/mount_validate.sh
+++ bin/mount_validate.sh
@@ -1,6 +1,10 @@
+echo 'Validating mount...'
+
 docker compose -f ./build/docker-compose.yaml \
-    run \
-    --volume $1:/data \
-    $2 \
+    run nitelite-pipeline \
     /bin/bash -c \
-    'ls /data'
+    'conda run -n nitelite-pipeline-conda python -c \
+    "import os; \
+os.listdir(\"/data\"); \
+os.listdir(\"/data/referenced_images\"); \
+os.listdir(\"/data/nitelite_pipelin_output\")"'
