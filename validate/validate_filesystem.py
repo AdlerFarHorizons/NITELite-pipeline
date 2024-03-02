50988daaa9242978c73428964a215aa21f6fce13
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2024-03-02 21:23:32 +0000

    Fix filepath.

--- validate/validate_filesystem.py
+++ validate/validate_filesystem.py
@@ -3,13 +3,13 @@ import os
 # Write
 fps = []
 for i in range(2):
-    fp = f'/data/nitelite_pipeline_output/test{i}.txt'
+    fp = f'/data/output/test{i}.txt'
     with open(fp, 'w') as f:
         f.write('Hello, world!\n')
     fps.append(fp)
 
 # Read
-os.listdir('/data/nitelite_pipeline_output/')
+os.listdir('/data/output/')
 for fp in fps:
     with open(fp, 'r') as f:
         print(f.read())
