eb833bd231985ba00a106ba96c7336de567eefe5
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2024-03-02 02:57:58 +0000

    Add parameter validation to get_service method

--- bin/validate.py
+++ bin/validate.py
@@ -21,11 +21,37 @@ args = parser.parse_args()
 print(args.config_filepath)
 print(args.validate_only)
 
+def get_service(self, name, *args, **kwargs):
+    '''
+    TODO: Add parameter validation.
+    '''
+
+    # Get parameters for constructing the service
+    constructor_dict = self._services.get(name)
+    if not constructor_dict:
+        raise ValueError(f'Service {name} not registered')
+
+    # Parse constructor parameters
+    if constructor_dict['singleton'] and name in self.services:
+        return self.services[name]
+    constructor = constructor_dict['constructor']
+
+    # Get the used arguments
+    if constructor_dict['args_key'] is None:
+        args_key = name
+    else:
+        args_key = constructor_dict['args_key']
+    kwargs = self.get_service_args(args_key, constructor, **kwargs)
+
+    return constructor, args, kwargs
+
+
 print('Before IOManager')
 from io_manager import IOManager
 from night_horizons.mapmake import create_mapmaker
 mapmaker = create_mapmaker(args.config_filepath)
-io_manager = mapmaker.container.get_service('io_manager')
+constructor, args, kwargs = mapmaker.container.get_service('io_manager')
+io_manager = IOManager(*args, **kwargs)
 
 # io_manager = IOManager(
 #     input_dir='/data/referenced_images',
