36c163340e5f5c3c4e4dfb4f882b470c670c2ed8
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2024-03-02 17:23:43 +0000

    Refactor to make a validate that takes less time.

--- validate/comprehensive_validate.sh
+++ validate/comprehensive_validate.sh
@@ -6,7 +6,7 @@ ls /data/
 echo
 
 echo 'Can we see the input and output buckets from inside the docker container inside the ec2 instance?'
-docker compose -f ./aws/docker-compose.yaml \
+docker compose -f ./validate/docker-compose.yaml \
     run nitelite-pipeline \
     /bin/bash -c \
     'ls /data/'
@@ -27,7 +27,7 @@ ls /data/nitelite_pipeline_output/
 echo
 
 echo 'Can we see the config file from inside the docker container inside the ec2 instance?'
-docker compose -f ./aws/docker-compose.yaml \
+docker compose -f ./validate/docker-compose.yaml \
     run \
     --volume $(realpath ./config/mosaic.yml):/used_config.yml \
     nitelite-pipeline \
@@ -36,7 +36,7 @@ docker compose -f ./aws/docker-compose.yaml \
 echo
 
 echo 'Does the conda environment inside the docker container work?'
-docker compose -f ./aws/docker-compose.yaml \
+docker compose -f ./validate/docker-compose.yaml \
     run nitelite-pipeline \
     /bin/bash -c \
     'conda run -n nitelite-pipeline-conda python -c \
@@ -45,16 +45,16 @@ print(f\"Found sys.executable at {sys.executable}!\");"'
 echo
 
 echo 'Can we see the input and output buckets from inside the conda environment inside the docker container inside the ec2 instance?'
-docker compose -f ./aws/docker-compose.yaml \
+docker compose -f ./validate/docker-compose.yaml \
     run nitelite-pipeline \
     /bin/bash -c \
     'conda run -n nitelite-pipeline-conda ls /data/'
 echo
 
 echo 'Can we create, see, and delete files inside the output bucket using Python?'
-docker compose -f ./aws/docker-compose.yaml \
+docker compose -f ./validate/docker-compose.yaml \
     run \
-    --volume $(realpath ./bin/validate_filesystem.py):/validate_filesystem.py \
+    --volume $(realpath ./validate/validate_filesystem.py):/validate_filesystem.py \
     nitelite-pipeline \
     /bin/bash -c \
     'conda run -n nitelite-pipeline-conda --live-stream \
@@ -62,17 +62,17 @@ docker compose -f ./aws/docker-compose.yaml \
 echo
 
 echo 'Is the code inside the docker container what we expect?'
-docker compose -f ./aws/docker-compose.yaml \
+docker compose -f ./validate/docker-compose.yaml \
     run nitelite-pipeline \
     /bin/bash -c \
     'pwd; echo "Contained files:"; ls ./night-horizons-mapmaker/night_horizons'
 echo
 
 echo 'Can we see the input and output buckets from inside a python script inside the conda environment inside the docker container inside the ec2 instance?'
-docker compose -f ./aws/docker-compose.yaml \
+docker compose -f ./validate/docker-compose.yaml \
     run \
-    --volume $(realpath ./bin/validate.py):/validate.py \
-    --volume $(realpath ./bin/io_manager.py):/io_manager.py \
+    --volume $(realpath ./validate/validate.py):/validate.py \
+    --volume $(realpath ./validate/io_manager.py):/io_manager.py \
     --volume $(realpath ./config/mosaic.yml):/used_config.yml \
     nitelite-pipeline \
     /bin/bash -c \
@@ -80,16 +80,16 @@ docker compose -f ./aws/docker-compose.yaml \
     python /validate.py /used_config.yml --validate_only'
 echo
 
-# echo 'Does the pipeline code inside the docker container find the data?'
-# docker compose -f ./aws/docker-compose.yaml \
-#     run \
-#     --volume $(realpath ./config/mosaic.yml):/used_config.yml \
-#     nitelite-pipeline \
-#     /bin/bash -c \
-#     'conda run -n nitelite-pipeline-conda \
-#     python night-horizons-mapmaker/night_horizons/mapmake.py \
-#     /used_config.yml --validate_only'
-# 
-# echo 'Does the execution script work?'
-# ./bin/mapmake.sh -d /data:/data -c $(realpath ./config/mosaic.yml):/used_config.yml --validate_only
-# echo
+echo 'Does the pipeline code inside the docker container find the data?'
+docker compose -f ./validate/docker-compose.yaml \
+    run \
+    --volume $(realpath ./config/mosaic.yml):/used_config.yml \
+    nitelite-pipeline \
+    /bin/bash -c \
+    'conda run -n nitelite-pipeline-conda \
+    python night-horizons-mapmaker/night_horizons/mapmake.py \
+    /used_config.yml --validate_only'
+
+echo 'Does the execution script work?'
+./validate/mapmake.sh -d /data:/data -c $(realpath ./config/mosaic.yml):/used_config.yml --validate_only
+echo
