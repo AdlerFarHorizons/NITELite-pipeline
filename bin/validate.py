e1500325cb5bca280f2a1044223b2d90d1a988fc
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2024-03-02 01:08:58 +0000

    Add print statement to validate method in validate.py

--- bin/validate.py
+++ bin/validate.py
@@ -28,17 +28,18 @@ def validate(self):
 
     print('Validating pipeline setup...')
     io_manager = self.container.get_service('io_manager')
+    print(io_manager)
 
-    print('Counting input filepaths...')
-    input_fp_count = {
-        key: len(val) for key, val
-        in io_manager.input_filepaths.items()
-    }
-    total_fp_count = 0
-    for key, count in input_fp_count.items():
-        print(f'    {key}: {count} filepaths')
-        total_fp_count += count
-    print(f'    ------------\n    Total: {total_fp_count} filepaths')
-    if total_fp_count == 0:
-        print('WARNING: No input filepaths found.')
+    # print('Counting input filepaths...')
+    # input_fp_count = {
+    #     key: len(val) for key, val
+    #     in io_manager.input_filepaths.items()
+    # }
+    # total_fp_count = 0
+    # for key, count in input_fp_count.items():
+    #     print(f'    {key}: {count} filepaths')
+    #     total_fp_count += count
+    # print(f'    ------------\n    Total: {total_fp_count} filepaths')
+    # if total_fp_count == 0:
+    #     print('WARNING: No input filepaths found.')
 validate(mapmaker)
