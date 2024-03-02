4f62d1fb87057622213c8d64c6862e3a5560686c
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2024-03-02 00:45:25 +0000

    Validation script update. Tracking down where things break.

--- bin/validate.py
+++ bin/validate.py
@@ -3,3 +3,14 @@ print('Testing...')
 import os
 print(os.getcwd())
 print(os.listdir('/data/'))
+
+# Set up the argparser
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    'config_filepath',
+    type=str,
+    help='Location of config file.',
+)
+args = parser.parse_args()
+print(args.config_filepath)
