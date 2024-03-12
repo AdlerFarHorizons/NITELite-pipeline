6dc30ec8fedd8758b297e0900e817dc064783174
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2024-03-12 20:23:36 +0000

    Remove commented out code in test.sh

--- test/test.sh
+++ test/test.sh
@@ -201,17 +201,3 @@ docker compose -f $COMPOSE_FILE \
 echo 'pytest.log:'
 cat ./night-horizons-mapmaker/test/pytest.log
 echo
-
-# echo 'Does the pipeline code inside the docker container find the data?'
-# docker compose -f $COMPOSE_FILE \
-#     run \
-#     --volume $CONFIG_FILEPATH:/used_config.yml \
-#     nitelite-pipeline \
-#     /bin/bash -c \
-#     'conda run -n nitelite-pipeline-conda \
-#     python night-horizons-mapmaker/night_horizons/mapmake.py \
-#     /used_config.yml --validate_only'
-# 
-# echo 'Does the execution script work?'
-# ./validate/mapmake.sh -d /data:/data -c $CONFIG_FILEPATH:/used_config.yml --validate_only
-# echo
