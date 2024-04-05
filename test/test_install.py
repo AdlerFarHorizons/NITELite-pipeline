877a3569e681f378120e60dfdfb36fd24d0c9363
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2024-04-05 13:52:28 -0500

    test_install now only checks for crucial packages.

--- test/test_install.py
+++ test/test_install.py
@@ -3,31 +3,38 @@ import json
 
 if __name__ == '__main__':
 
-    filepath = '/installed_dependencies.json'
-
+    filepath = '/conda-env.yaml'
     with open(filepath, 'r') as f:
-        packages = json.load(f)
+        lines = f.readlines()
 
-    skipped = []
+    with open('module_names.json') as f:
+        module_names = json.load(f)
+
+    in_dependencies = False
     failed = []
-    for package in packages:
-        name = package['name']
+    for line in lines:
 
-        # Skip packages with dashes
-        if '-' in name:
-            skipped.append(name)
+        # Skip until we get to dependencies
+        if line == 'dependencies:':
+            in_dependencies = True
+        if not in_dependencies:
             continue
+
+        # Get the name of the module to import
+        module = line.split('  - ')[1]
+        if module in module_names:
+            module = module_names[module]
 
         # Test import
         try:
-            importlib.import_module(name)
+            importlib.import_module(module)
         except ImportError:
-            failed.append(name)
+            failed.append(module)
 
     # Check for key dependencies
     if len(failed) > 0:
         raise ImportError(
             'Could not import all modules.\n'
-            f'{len(packages)} attempts, {len(failed)} failures.\n'
+            f'{len(lines)} attempts, {len(failed)} failures.\n'
             f'Failures: {failed}'
         )
