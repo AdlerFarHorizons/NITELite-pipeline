a962fa702907e43065453678a0a02a4baafddcc0
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2024-04-05 13:26:50 -0500

    Built out test_install and the call for it.

--- test/test_install.py
+++ test/test_install.py
@@ -1,3 +1,5 @@
+import importlib
+
 if __name__ == '__main__':
 
     filepath = '/installed_dependencies.txt'
@@ -5,4 +7,25 @@ if __name__ == '__main__':
     with open(filepath, 'r') as f:
         lines = f.readlines()
 
-    print(lines[0])
+    skipped = []
+    failed = []
+    for line in lines:
+        package = line.split('=')[0]
+
+        # Skip packages with dashes
+        if '-' in package:
+            skipped.append(line)
+            continue
+
+        # Test import
+        try:
+            importlib.import_module(package)
+        except ImportError:
+            failed.append(package)
+
+    if len(failed) > 0:
+        raise ImportError(
+            'Could not import all modules.\n'
+            f'{len(lines)} attempts, {len(failed)} failures.\n'
+            f'Failures: {failed}'
+        )
