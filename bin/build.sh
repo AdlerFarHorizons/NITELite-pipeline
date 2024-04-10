60ef8235c300e5d83c09a7349baad996be9a89d4
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2024-04-10 15:46:59 -0500

    build.sh doesn't assume AWS is the goal now.

--- bin/build.sh
+++ bin/build.sh
@@ -2,8 +2,13 @@
 
 # Build the image
 docker build --progress plain -f ./build/Dockerfile \
-    -t nitelite-pipeline:latest --platform linux/amd64 "$@" .
+    -t nitelite-pipeline:latest "$@" .
 
+# Below is code for pushing to AWS
+# # Build the image
+# docker build --progress plain -f ./build/Dockerfile \
+#     -t nitelite-pipeline:latest --platform linux/amd64 "$@" .
+# 
 # # Log in to AWS
 # aws ecr-public get-login-password --region us-east-1 | \
 #     docker login --username AWS --password-stdin public.ecr.aws/i2m6n0b5
