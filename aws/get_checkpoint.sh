afcd6b1137bf72d67dc5aa3f19d5be1d0c1c5949
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2024-04-16 16:35:53 -0500

    Added some exclusions.

--- aws/get_checkpoint.sh
+++ aws/get_checkpoint.sh
@@ -2,7 +2,7 @@ if aws s3 ls $CHECKPOINTED_OUTPUT | grep -q "PRE"; then
     echo "Found checkpointed output at $CHECKPOINTED_OUTPUT"
     aws s3 cp $CHECKPOINTED_OUTPUT ./data/output/ \
         --recursive \
-        --exclude "referenced_images/*"
+        --exclude "referenced_images/*" --exclude "checkpoints/mosaic_i*"
 else
     echo "No checkpointed output found at $CHECKPOINTED_OUTPUT"
 fi
