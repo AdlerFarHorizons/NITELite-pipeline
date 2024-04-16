96937c02ce4c649665336f2de1619488609839e2
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2024-04-16 16:32:59 -0500

    Fixed a bug in get_checkpoint

--- aws/get_checkpoint.sh
+++ aws/get_checkpoint.sh
@@ -1,5 +1,8 @@
-if [ -z "$(aws s3 ls $CHECKPOINTED_OUTPUT)" ]; then
+if aws s3 ls $CHECKPOINTED_OUTPUT | grep -q "PRE"; then
+    echo "Found checkpointed output at $CHECKPOINTED_OUTPUT"
     aws s3 cp $CHECKPOINTED_OUTPUT ./data/output/ \
         --recursive \
         --exclude "referenced_images/*"
+else
+    echo "No checkpointed output found at $CHECKPOINTED_OUTPUT"
 fi
