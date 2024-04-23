5f4da495f50838065add3d8b2e5797e0c971e41a
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2024-04-23 19:57:09 +0000

    Tweaks to existing_output.

--- aws/get_checkpoint.sh
+++ aws/get_checkpoint.sh
@@ -1,8 +1,8 @@
-if aws s3 ls $CHECKPOINTED_OUTPUT | grep -q "PRE"; then
-    echo "Found checkpointed output at $CHECKPOINTED_OUTPUT"
-    aws s3 cp $CHECKPOINTED_OUTPUT ./data/output/ \
+if aws s3 ls $EXISTING_OUTPUT | grep -q "PRE"; then
+    echo "Found checkpointed output at $EXISTING_OUTPUT"
+    aws s3 cp $EXISTING_OUTPUT ./data/output/ \
         --recursive \
         --exclude "referenced_images/*" --exclude "checkpoints/mosaic_i*"
 else
-    echo "No checkpointed output found at $CHECKPOINTED_OUTPUT"
+    echo "No checkpointed output found at $EXISTING_OUTPUT"
 fi
