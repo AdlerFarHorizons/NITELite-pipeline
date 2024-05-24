e7c3dc5992e8252981e22e9584ebd2528a3df517
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2024-05-24 09:46:16 -0500

    Changed mount names.

--- aws/mount_buckets.sh
+++ aws/mount_buckets.sh
@@ -6,11 +6,11 @@ mkdir /data/input
 mkdir /data/output
 
 # Specific input directories
-mkdir /data/input/referenced_images
-mount-s3 nitelite.referenced-images /data/input/referenced_images
+mkdir /data/input/nitelite.referenced-images
+mount-s3 nitelite.referenced-images /data/input/nitelite.referenced-images
 mkdir /data/input/metadata
-mount-s3 nitelite.metadata /data/input/metadata
-mkdir /data/input/images
-mount-s3 nitelite.images /data/input/images
-mkdir /data/input/existing_output
-mount-s3 nitelite.pipeline-output /data/input/existing_output
+mount-s3 nitelite.metadata /data/input/nitelite.metadata
+mkdir /data/input/nitelite.images
+mount-s3 nitelite.images /data/input/nitelite.images
+mkdir /data/input/nitelite.pipeline-output
+mount-s3 nitelite.pipeline-output /data/input/nitelite.pipeline-output
